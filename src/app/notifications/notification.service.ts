b05eab24fe3499eb8c0ea7324e836754c989a43e
Luisj950 <lajaramillo@sudamericano.edu.ec>
2025-06-08 01:18:13 -0500

    segundo fronted

--- src/app/notifications/notification.service.ts
+++ src/app/notifications/notification.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NotificationService {
-  private apiUrl = 'http://localhost:3000/notifications';
+  private apiUrl = 'https://notification-api.onrender.com/notifications';
 
   constructor(private http: HttpClient) {}
 
